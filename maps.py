7d3ebc531ad4a399699492d0b89fd3d3113d2efc
Mahadhevha <m.mahadhevha@gmail.com>
2021-08-29 20:21:22 +0800

    A simplified Mapping program.

---
+++ maps.py
@@ -0,0 +1,89 @@
+import osmnx as ox
+from geopy.geocoders import Nominatim
+
+
+print("Enter your City to map out: ")
+City = input()
+print("Enter your City to map out: ")
+Country = input()
+
+# instantiate a new Nominatim client
+app = Nominatim(user_agent="tutorial")
+# get location coordinates data
+location = app.geocode(City).raw
+latitude = location["lat"]
+longitude = location["lon"]
+
+place = [City, Country]
+G = ox.graph_from_place(place, retain_all=True,
+                        simplify=True, network_type='all')
+
+###############################################################################
+#                           Unpacking the Data                                #
+###############################################################################
+u = []
+v = []
+key = []
+data = []
+for uu, vv, kkey, ddata in G.edges(keys=True, data=True):
+    u.append(uu)
+    v.append(vv)
+    key.append(kkey)
+    data.append(ddata)
+
+# Lists to store colors and widths
+roadColors = []
+roadWidths = []
+
+for item in data:
+    if "length" in item.keys():
+        if item["length"] <= 100:
+            linewidth = 0.10
+            color = "#a6a6a6"
+
+        elif item["length"] > 100 and item["length"] <= 200:
+            linewidth = 0.15
+            color = "#676767"
+
+        elif item["length"] > 200 and item["length"] <= 400:
+            linewidth = 0.25
+            color = "#454545"
+
+        elif item["length"] > 400 and item["length"] <= 800:
+            color = "#bdbdbd"
+            linewidth = 0.35
+        else:
+            color = "#d5d5d5"
+            linewidth = 0.45
+
+        if "primary" in item["highway"]:
+            linewidth = 0.5
+            color = "#ffff"
+    else:
+        color = "#a6a6a6"
+        linewidth = 0.10
+
+    roadColors.append(color)
+    roadWidths.append(linewidth)
+
+for item in data:
+    if "footway" in item["highway"]:
+        color = "#ededed"
+        linewidth = 0.25
+    else:
+        color = "#a6a6a6"
+        linewidth = 0.5
+
+    roadWidths.append(linewidth)
+
+bgcolor = "#061529"
+
+#Load and save the image
+fig, ax = ox.plot_graph(G, node_size=0, figsize=(27, 30),
+                        dpi=300, bgcolor=bgcolor,
+                        save=False, edge_color=roadColors,
+                        edge_linewidth=roadWidths, edge_alpha=1)
+
+fig.tight_layout(pad=0)
+fig.savefig("Figure1.png", dpi=300, format="png", bbox_inches='tight',
+            facecolor=fig.get_facecolor(), transparent=False)
